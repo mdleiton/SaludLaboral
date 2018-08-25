fd3b4bf2ce89b5e5c4c617ea3f67aa68288f9509
mdleiton <mau_leiton96@hotmail.com>
2018-08-25 13:18:39 -0500

    [ADD] interfaz  1/s

--- trabajo_saluble/src/trabajo_saluble/core.clj
+++ trabajo_saluble/src/trabajo_saluble/core.clj
@@ -1,6 +1,10 @@
 (ns trabajo-saluble.core
   (:require [svm.core :as svm])
-    (:gen-class))
+  (:import (javax.swing SwingUtilities JFrame JLabel JTextField JButton JPanel JOptionPane  JTextArea)
+           (java.awt BorderLayout)
+           (java.awt FlowLayout)
+           (java.awt.event ActionListener))
+  (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -20,4 +24,43 @@
 	; Label it.
 	(svm/predict model feature)
 	;=> 1.0
+
+    ;Interfaz 
+  (let [f (JFrame. "Asignación estratégica de tiempo de descanso")
+        labeltitle (JLabel. "**********Sistema basado en parámetros ambientales del ambiente laboral.*********")
+        labelhumedad (JLabel. "Humedad:")
+        labeltemperatura (JLabel. "Temperatura:")
+        labelruido (JLabel. "Ruido:")
+        txthumedad (JTextField. "" 25)
+        txttemperatura (JTextField. "" 25)
+        txtruido (JTextField. "" 25)        
+        buttonverificacion (JButton. "Verificar condicional" )
+        labelsugerencias (JLabel. "Segerencias:")
+        labelestadoactual (JLabel. "Estado Actual:")
+        textarea ( JTextArea. "" )
+        panel (JPanel.) panel1 (JPanel.)
+        f (proxy [JFrame ActionListener]
+        [] ; 
+        (actionPerformed [e] ; nil below is the parent component
+        (JOptionPane/showMessageDialog
+           nil (str "agregar funcion de predicion"))))]
+    
+    (doto panel
+      (.add labeltitle)
+      (.add labelsugerencias) (.add labelhumedad) (.add txthumedad) (.add labelsugerencias)
+      (.add labeltemperatura) (.add txttemperatura) (.add textarea)
+      (.add labelruido) (.add txtruido)
+      (.add buttonverificacion))
+    
+    (doto f
+      (.setContentPane panel)    
+      (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
+      (.pack)
+      (.setSize 500 300)
+      (. setResizable true)
+      (.setVisible true))
+    (.addActionListener buttonverificacion f)
+  )
+
+
 	)
