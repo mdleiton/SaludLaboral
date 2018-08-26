e5413e282144fe8762eefd955ce02c5a98098ac3
mdleiton <mau_leiton96@hotmail.com>
2018-08-25 23:34:26 -0500

    [upd] interfaz inicio

--- trabajo_saluble/src/trabajo_saluble/core.clj
+++ trabajo_saluble/src/trabajo_saluble/core.clj
@@ -3,93 +3,79 @@
   (:import (javax.swing SwingUtilities JFrame JLabel JTextField JButton JPanel JOptionPane  JTextArea)
            (java.awt BorderLayout)
            (java.awt FlowLayout)
-           (java.awt.event ActionListener))
+           (java.awt.event ActionListener)
+           (java.awt GridLayout))
   (:gen-class))
 
 (defn -main [& args]
   
     ; Load the heart scale example dataset.
-    (def dataset (svm/read-dataset "data/entrenamiento"))
+    (def dataset (svm/read-dataset "data/prueba"))
 
     ; Train a SVM model.
     (def model (svm/train-model dataset))
 
     ; Get the feature map you want to predict.
-    (def datasetDePrueba (svm/read-dataset "data/prueba"))
+    ;(def datasetDePrueba (svm/read-dataset "data/prueba"))
     
-    (def counter (atom 0))
-    (defn inc-counter []
-    (swap! counter inc))
+    ;(def counter (atom 0))
+    ;(defn inc-counter []
+    ;(swap! counter inc))
 
     ; (for [x datasetDePrueba] )
 
-    (def feature (last (first datasetDePrueba)))
+    ;(def feature (last (first datasetDePrueba)))
     ;=> {1 0.708333, 2 1.0, 3 1.0, ...}
 
-    (print (first datasetDePrueba))
-    (let [[x y] (first datasetDePrueba)]
-        (print x))
+    ;(print (first datasetDePrueba))
+    ;(let [[x y] (first datasetDePrueba)]
+     ;   (print x))
 
     ; Label it.
-    ; (print (svm/predict model feature))
+    ; )
     ;=> 1.0
 
     ;Interfaz 
 
     (let [  f (JFrame. "Tiempo de Descanso")
           ; labeltitle (JLabel. "**********Sistema basado en parámetros ambientales del ambiente laboral.*********")
-          labelhumedad (JLabel. "Humedad:")
+          labelhumedad (JLabel. "Humedad:    ")
           labeltemperatura (JLabel. "Temperatura:")
-          labelruido (JLabel. "Ruido:")
+          labelruido (JLabel. "Ruido:      ")
           txttemperatura (JTextField. "" 25)
           txthumedad (JTextField. "" 25)
           txtruido (JTextField. "" 25)        
-          buttonverificacion (JButton. "Verificar condiones" )        
-  
-          panel (JPanel.)
-          panelTemperatura (JPanel.)
-          panelHumedad (JPanel.)
-          panelRuido (JPanel.)
-          panelBoton (JPanel.)
+          buttonverificacion (JButton. "Verificar condiciones" )    
+          labelestadoactual(JLabel. "Estado actual:")
+          
+
           f (proxy [JFrame ActionListener]
           [] ; 
   
           (actionPerformed [e] ; nil below is the parent component
             (JOptionPane/showMessageDialog
-             nil  (str "agregar funcion de predicion")                  
+             nil  (str (svm/predict model {1 (Double/parseDouble (.getText txttemperatura)), 2 (Double/parseDouble(.getText txtruido)), 3 (Double/parseDouble(.getText txthumedad)) }))                  
                  )))]
 
-        (doto panelTemperatura
-          (.add labeltemperatura)
-          (.add txttemperatura))
-
-        (doto panelHumedad
-          (.add labelhumedad)
-          (.add txthumedad))
-
-        (doto panelRuido
-          (.add labelruido)
-          (.add txtruido))
-        
-        (doto panelBoton
-            (.add buttonverificacion))
-
-        (doto panel
-            (.add panelTemperatura)
-            (.add panelHumedad)
-            (.add panelRuido)
-            (.add panelBoton))   
-        
         (doto f
-          (.setContentPane panel)    
+          (.setLayout (GridLayout. 4 2 25 10))
+          (.add labeltemperatura)
+          (.add txttemperatura)  
+          (.add labelruido)
+          (.add txtruido)
+          (.add labelhumedad)
+          (.add txthumedad)
+          (.add buttonverificacion)
+          (.add labelestadoactual)
+          
           (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
-          (.pack)
+          (.setResizable false)
           (.setVisible true))
-
-        (.setSize f 400 150)
-
+        (.setTitle f "sistema de predicción")
+        (.setSize f 450 250)
         (.addActionListener buttonverificacion f)
        
         f 
     )
+   
 )
